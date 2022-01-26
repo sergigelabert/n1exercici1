fe1442ba526315db66556faa224505eda5ac17c5
sergi <sergiceballosfeina@gmail.com>
2022-01-26 09:32:01 +0100

    n1exercici1

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,14 @@
+/*Crea una classe Vehicle amb un constructor i un mètode anomenat iniciar(),
+ que es invocat pel main() (aquest mètode pot estar en la mateixa classe o en una classe apart) .
+ Demostra la seqüència d'execució dels mètodes de la classe (no entenc què vol dir això).
+ */
+public class Vehicle {
+    public static void iniciar(){
+
+    }
+    public static void main(String[] args) {
+        iniciar();
+        int iniciar = 0;
+        System.out.println(iniciar);
+    }
+}
